6f9da40e4a0f5826fb88a353202bb0623f504a33
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-07-14 12:14:47 +0200

    Updates

---
+++ precache-manifest.d78fafb3cd57c4fca6d1ab27c18985e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556f16826d924ede2cab08fd458c5429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cd4ebddb1d8f8a43026",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "108e8f15bd9bdaba7332",
+    "url": "/static/js/2.e20ac7af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e20ac7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd4ebddb1d8f8a43026",
+    "url": "/static/js/main.ef72f1ad.chunk.js"
+  },
+  {
+    "revision": "56d162087811e3780ef1",
+    "url": "/static/js/runtime-main.3acd35cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
